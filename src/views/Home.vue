7e027bc2f919dff65d595a8b7fd1e52a6af0b7d9
JAEHO SONG <thruthesky@gmail.com>
2020-10-06 23:33:24 +0900

    update for link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="$router.push('/contact/apple/banana')"> <!-- 프로그램적으로 페이지 이동 -->
+      Move to Contact page
+    </button>
   </div>
 </template>
 
